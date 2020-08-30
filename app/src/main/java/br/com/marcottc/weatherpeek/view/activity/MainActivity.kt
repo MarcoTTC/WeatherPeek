de09bdb643a2fe50db74f66a15a1c61e83b06c62
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2020-08-30 13:32:23 -0300

    Adding Main Activity

---
+++ app/src/main/java/br/com/marcottc/weatherpeek/view/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.marcottc.weatherpeek.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.marcottc.weatherpeek.R
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
