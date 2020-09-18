15109f686dcdd8e4160198daf5b13fb738cb86a2
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2020-09-18 16:34:27 -0300

    Adding a message in OneCallAppId.kt

--- app/src/main/java/br/com/marcottc/weatherpeek/util/OneCallAppId.kt
+++ app/src/main/java/br/com/marcottc/weatherpeek/util/OneCallAppId.kt
@@ -3,6 +3,8 @@ package br.com.marcottc.weatherpeek.util
 class OneCallAppId {
 
     companion object {
+        // If this val is an empty string, you must access "https://openweathermap.org/api/one-call-api",
+        // register yourself and account, and add your app id in this val
         const val appId: String = ""
     }
 }
