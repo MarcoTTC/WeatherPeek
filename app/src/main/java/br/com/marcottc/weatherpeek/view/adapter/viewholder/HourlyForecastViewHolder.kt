d1ece0903ab257dd28ea99a4054db654099ebba2
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2020-09-19 17:44:12 -0300

    Moving the string 'NOW' to the resources file

--- app/src/main/java/br/com/marcottc/weatherpeek/view/adapter/viewholder/HourlyForecastViewHolder.kt
+++ app/src/main/java/br/com/marcottc/weatherpeek/view/adapter/viewholder/HourlyForecastViewHolder.kt
@@ -3,18 +3,31 @@ package br.com.marcottc.weatherpeek.view.adapter.viewholder
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import br.com.marcottc.weatherpeek.R
 import br.com.marcottc.weatherpeek.databinding.ItemHourlyForecastBinding
 import br.com.marcottc.weatherpeek.model.HourlyWeatherData
 
-class HourlyForecastViewHolder(private var binding: ItemHourlyForecastBinding) : RecyclerView.ViewHolder(binding.root) {
+class HourlyForecastViewHolder(private var binding: ItemHourlyForecastBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 
     fun bind(hourlyData: HourlyWeatherData) {
         binding.tempValue.text = String.format("%.0f°c", hourlyData.temp)
-        if (String.format("%te%tH", System.currentTimeMillis(), System.currentTimeMillis()) == String.format("%te%tH", hourlyData.dt*1000, hourlyData.dt*1000)) {
-            binding.hourValue.text = "NOW"
+
+        if (String.format(
+                "%te%tH",
+                System.currentTimeMillis(),
+                System.currentTimeMillis()
+            ) == String.format("%te%tH", hourlyData.dt * 1000, hourlyData.dt * 1000)
+        ) {
+            binding.hourValue.text = binding.root.context.resources.getString(R.string.now_label)
         } else {
             binding.hourValue.text =
-                String.format("%tH:%tM%Tp", hourlyData.dt*1000, hourlyData.dt*1000, hourlyData.dt*1000)
+                String.format(
+                    "%tH:%tM%Tp",
+                    hourlyData.dt * 1000,
+                    hourlyData.dt * 1000,
+                    hourlyData.dt * 1000
+                )
         }
     }
 
