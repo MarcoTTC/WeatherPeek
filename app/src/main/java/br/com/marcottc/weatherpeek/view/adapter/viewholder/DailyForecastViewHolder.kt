f89f5c1ef296df856ac1c74adfba415444e4049f
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2020-09-21 21:14:29 -0300

    Updating the daily forecast adapter and viewholder

--- app/src/main/java/br/com/marcottc/weatherpeek/view/adapter/viewholder/DailyForecastViewHolder.kt
+++ app/src/main/java/br/com/marcottc/weatherpeek/view/adapter/viewholder/DailyForecastViewHolder.kt
@@ -5,11 +5,19 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.marcottc.weatherpeek.databinding.ItemDailyForecastBinding
 import br.com.marcottc.weatherpeek.model.DailyWeatherData
+import com.bumptech.glide.Glide
 
 class DailyForecastViewHolder(private var binding: ItemDailyForecastBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(dailyData: DailyWeatherData) {
-        binding.dayValue.text = String.format("%Ta", dailyData.dt)
+        binding.dayValue.text = String.format("%Ta", dailyData.dt*1000)
+        val resources = binding.root.context.resources
+        val iconRatio = (resources.displayMetrics.density * 48).toInt()
+        Glide.with(binding.root)
+            .load(dailyData.weatherList[0].getIconUrl())
+            .override(iconRatio, iconRatio)
+            .centerInside()
+            .into(binding.weatherIcon)
         binding.maxTempValue.text = String.format("%.0f°", dailyData.temp.max)
         binding.minTempValue.text = String.format("%.0f°", dailyData.temp.min)
     }
