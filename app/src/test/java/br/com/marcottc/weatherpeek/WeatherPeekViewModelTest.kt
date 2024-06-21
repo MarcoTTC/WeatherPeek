b365438c970846fed81024adce2ba07d1b9cc935
Marco Coelho <marco.tulio.todeschini.coelho@gmail.com>
2024-06-20 22:20:47 -0300

    Adding some basic error test scenarios for the view model

--- app/src/test/java/br/com/marcottc/weatherpeek/WeatherPeekViewModelTest.kt
+++ app/src/test/java/br/com/marcottc/weatherpeek/WeatherPeekViewModelTest.kt
@@ -10,7 +10,10 @@ import br.com.marcottc.weatherpeek.util.AppKeyUtil
 import br.com.marcottc.weatherpeek.util.NetworkUtil
 import br.com.marcottc.weatherpeek.util.PermissionUtil
 import br.com.marcottc.weatherpeek.viewmodel.WeatherPeekViewModel
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -47,7 +50,54 @@ class WeatherPeekViewModelTest {
     }
 
     @Test
-    fun getWeatherData_noAppIdError() {
-        fail()
+    fun weatherPeekViewModel_isInitialized() {
+        assertEquals(viewModel.viewModelState.value, WeatherPeekViewModel.State.LOADING)
+        assertEquals(viewModel.mustRequestPermissionFirst.value, false)
+        assertEquals(viewModel.requestingWeatherData.value, false)
+        assertEquals(viewModel.showMessage.value, "")
+    }
+
+    @Test
+    fun getWeatherData_noAppId_showsErrorMessage() {
+        runBlocking {
+            coEvery { appKeyUtil.isEmpty() } returns true
+            viewModel.getWeatherData()
+        }
+        assertEquals(viewModel.showMessage.value, "Please set up an app id before building the project!")
+        assertEquals(viewModel.requestingWeatherData.value, false)
+        assertEquals(viewModel.viewModelState.value, WeatherPeekViewModel.State.FAILED)
+        coVerify { appKeyUtil.isEmpty() }
+    }
+
+    @Test
+    fun getWeatherData_noInternetConnectivity_showsErrorMessage() {
+        runBlocking {
+            coEvery { appKeyUtil.isEmpty() } returns false
+            coEvery { networkUtil.hasConnectivity() } returns false
+            coEvery { resources.getString(R.string.no_internet_connectivity) } returns "No internet connectivity!"
+            viewModel.getWeatherData()
+        }
+        assertEquals(viewModel.showMessage.value, "No internet connectivity!")
+        assertEquals(viewModel.requestingWeatherData.value, false)
+        assertEquals(viewModel.viewModelState.value, WeatherPeekViewModel.State.FAILED)
+        coVerify { appKeyUtil.isEmpty() }
+        coVerify { networkUtil.hasConnectivity() }
+        coVerify { resources.getString(R.string.no_internet_connectivity) }
+    }
+
+    @Test
+    fun getWeatherData_noLocationPermission_showsErrorMessage() {
+        runBlocking {
+            coEvery { appKeyUtil.isEmpty() } returns false
+            coEvery { networkUtil.hasConnectivity() } returns true
+            coEvery { permissionUtil.hasLocationPermission() } returns false
+            viewModel.getWeatherData()
+        }
+        assertEquals(viewModel.mustRequestPermissionFirst.value, true)
+        assertEquals(viewModel.requestingWeatherData.value, false)
+        assertEquals(viewModel.viewModelState.value, WeatherPeekViewModel.State.FAILED)
+        coVerify { appKeyUtil.isEmpty() }
+        coVerify { networkUtil.hasConnectivity() }
+        coVerify { permissionUtil.hasLocationPermission() }
     }
 }
